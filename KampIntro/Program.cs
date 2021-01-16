1f16f73bf845d7d91db8188604b5c5824b7e0812
Ramis Yuksel <ramisyk97@gmail.com>
2021-01-16 21:55:47 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            Console.WriteLine(kategoriEtiketi);
+            string[] arr = { "1", "2", "3" };
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+            }
+
+
+        }
+    }
+}
+//Don't Repeat Yourself - DRY
