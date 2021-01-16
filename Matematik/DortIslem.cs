41d6c6a0890780daf3fa6444e36210a5824774de
Ramis Yuksel <ramisyk97@gmail.com>
2021-01-16 23:28:38 +0300

    değer ve referans tipler incelendi

---
+++ Matematik/DortIslem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matematik
+{
+    class DortIslem
+    {
+        public void Topla()
+        {
+
+        }
+    }
+}
