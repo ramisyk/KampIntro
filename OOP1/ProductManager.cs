e09f5b986c236b1f45fd5fa70347491f3b238fbe
Ramis Yuksel <ramisyk97@gmail.com>
2021-01-20 22:30:31 +0300

    Ürün özelliklerinin bulunduğu Product ve Ürün ile ilgili methodların içerildiği ProductManager sınıfları hazırlanmıştır

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //void: işlem yapacak ancak sonucunda bir bilgiye ihtiyaç duymayan olaylarda (değer döndürmeyen)
+        public void Add(Product product) 
+        {
+            Console.WriteLine("Ürün eklendi : " + product.ProductName);
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine("Ürün güncellendi : " + product.ProductName);
+        }
+    }
+}
